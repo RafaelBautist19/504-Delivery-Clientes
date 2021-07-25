3f3a247cd7728e3c96594489441a65628a3dde3b
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-07-25 14:33:40 -0600

    HTML de los componentes Login,Landing y Signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './components/login/login.component';
 import {LandingComponent} from './components/landing/landing.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 const routes: Routes = [
-  { path:'', component: LandingComponent}
+  { path:'', component: LandingComponent},
+  { path:'login', component: LoginComponent},
+  { path:'signup', component: SignupComponent}
 ]
 
 
