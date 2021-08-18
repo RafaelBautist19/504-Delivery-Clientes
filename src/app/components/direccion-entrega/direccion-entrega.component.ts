9c573117b422170fb32802ede8a78680cfb3a1f7
LPSoldier19 <rafael.bautista1@hotmail.es>
2021-08-17 23:23:45 -0600

    Pagina de realizar pedido lista para unirla con el backend

--- src/app/components/direccion-entrega/direccion-entrega.component.ts
+++ src/app/components/direccion-entrega/direccion-entrega.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
+import { ClientesService } from 'src/app/services/clientes.service';
+
 
 @Component({
   selector: 'app-direccion-entrega',
@@ -14,6 +16,16 @@ export class DireccionEntregaComponent implements OnInit {
   lng:number = 0;
 
   ubicacionEntrega:any;
+
+  producto:any;
+
+  clienteActual:any = localStorage.getItem('clientID');
+  cliente:any;
+
+  envio:number = 30;
+  monto:number = 0;
+
+  constructor(private clientesService:ClientesService){}
 
   opcionSeleccionada:string = '';
   
@@ -31,6 +43,18 @@ export class DireccionEntregaComponent implements OnInit {
   private map!: google.maps.Map;
 
   ngOnInit(): void {
+
+    this.clientesService.informacionCliente(this.clienteActual).subscribe(
+      res=>{
+        this.cliente = res;
+      },
+      error=>{
+        console.log(error);
+      }
+    )
+
+    this.producto=JSON.parse(localStorage.getItem('productos')!);
+
     let loader = new Loader({
       apiKey: 'AIzaSyCBN7dfD0vWowUlEaDGg9zY_47VRnDbJkc'
     })
@@ -61,7 +85,20 @@ export class DireccionEntregaComponent implements OnInit {
     if(this.opcionSeleccionada === ''){
       alert('Seleccione un metodo de Pago');
     }
-    console.log(this.opcionSeleccionada, this.ubicacionEntrega);
+    
+    var clientePedido={
+      nombreCliente: `${this.cliente.nombre} ${this.cliente.apellido}`,
+      telefono: this.cliente.telefono,
+      idCliente: this.cliente._id
+    }
+    
+    console.log('Metodo de Pago:', this.opcionSeleccionada);
+    console.log('Direccion:', this.ubicacionEntrega);
+    console.log('Producto:', this.producto);
+    console.log('Cliente:', clientePedido);
+    console.log('Envio:', this.envio);
+    console.log('Monto:', this.envio + (this.producto.precio * this.producto.cantidad));
+
   }
 
 }
